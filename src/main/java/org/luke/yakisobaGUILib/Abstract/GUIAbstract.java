3fba800f3663f9112515a042d5bcc63a346cc26c
luke0630 <ryuto-0915@outlook.jp>
2024-08-18 00:32:32 +0900

    add: GUIAbstract

---
+++ src/main/java/org/luke/yakisobaGUILib/Abstract/GUIAbstract.java
@@ -0,0 +1,11 @@
+package org.luke.yakisobaGUILib.Abstract;
+
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+
+public abstract class GUIAbstract<E extends Enum<E>> {
+    public abstract String getGUITitle();
+    public abstract Enum<E> getType();
+    public abstract Inventory getInventory();
+    public abstract void InventoryClickListener(InventoryClickEvent event);
+}

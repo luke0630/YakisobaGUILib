8550832d6de36db0135e45900f3a04d0b81259d2
luke0630 <ryuto-0915@outlook.jp>
2024-12-20 01:44:58 +0900

    add: プレイヤー変数

--- src/main/java/org/luke/yakisobaGUILib/Abstract/GUIAbstract.java
+++ src/main/java/org/luke/yakisobaGUILib/Abstract/GUIAbstract.java
@@ -5,6 +5,8 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 
 public abstract class GUIAbstract<E extends Enum<E>> {
+    public Player player = null;
+
     public abstract E getType();
     public abstract Inventory getInventory(Player player);
     public abstract void InventoryClickListener(InventoryClickEvent event);
